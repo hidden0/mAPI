b2ead92d44fd452edc1ef3ff6a04a469ffda1874
John Thorne <john.thorne@John-Thorne-MBP.local>
2019-12-16 14:11:01 -0800

    Testing JSON object update.

--- check.py
+++ check.py
@@ -38,7 +38,7 @@ def buildDashboards(orgName, orgId, totalDev):
 	dashboard['panels'][0]['targets'][0]['y'] = 6
 	dashboard['panels'][0]['targets'][0]['rawSql'] = "SELECT\n  datecreated AS \"time\",\n  CONCAT(organization_name, ' Online') AS metric,\n  numonline\nFROM mnode_stats\nWHERE organization_name = '"+orgName.strip()+"'\nORDER BY 1,2"
 	dashboard['panels'][0]['targets'][0]['refId'] = "A"
-	dashboard['panels'][0]['targets'] = None
+	dashboard['panels'][0]['targets'] = json.loads("")
 
 	targetTmpA = {
 		"rawSql":"SELECT\n  datecreated AS \"time\",\n  CONCAT(organization_name, \' Online\') AS metric,\n  numonline\nFROM mnode_stats\nWHERE organization_name = \'"+orgName.strip()+"\'\nORDER BY 1,2",
