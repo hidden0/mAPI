dbc1c17ee0703b4382cc95bff05c827fc8274fa0
John Thorne <john.thorne@John-Thorne-MBP.local>
2019-12-17 14:47:10 -0800

    Outage simulation rapid testing.

--- check.py
+++ check.py
@@ -380,7 +380,7 @@ for org in orgJson:
 	else:
 		# Outage simulator (~8% chance)
 		seed(time.time())
-		outageChance = randint(0.100)
+		outageChance = randint(0,100)
 		orgHit = randint(1,4)
 
 		if mOrganization.organization_id==1:
@@ -390,17 +390,17 @@ for org in orgJson:
 				devStatus=fuzzNodeData(100, 1, 0.80, 0.12, 0.08)
 		elif mOrganization.organization_id==2:
 			if outageChance > 92 and orgHit==2:
-				devStatus=fuzzNodeData(100, 1, 0.0, 0.3, 0.7)
+				devStatus=fuzzNodeData(200, 1, 0.0, 0.3, 0.7)
 			else:
 				devStatus=fuzzNodeData(200, 1, 0.98, 0.0, 0.02)
 		elif mOrganization.organization_id==3:
 			if outageChance > 92 and orgHit==3:
-				devStatus=fuzzNodeData(100, 1, 0.0, 0.3, 0.7)
+				devStatus=fuzzNodeData(300, 1, 0.0, 0.3, 0.7)
 			else:
 				devStatus=fuzzNodeData(300, 1, 0.72, 0.2, 0.08)
 		elif mOrganization.organization_id==4:
 			if outageChance > 92 and orgHit==4:
-				devStatus=fuzzNodeData(100, 1, 0.0, 0.3, 0.7)
+				devStatus=fuzzNodeData(500, 1, 0.0, 0.3, 0.7)
 			else:
 				devStatus=fuzzNodeData(500, 1, 0.93, 0.02, 0.05)
 	totalDevices = len(devStatus)
